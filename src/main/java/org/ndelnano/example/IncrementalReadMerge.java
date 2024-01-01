456ab3e44d470a766038937d1b4b447f0bd205b8
Nick <nickdelnano@gmail.com>
2023-12-31 17:47:04 -0800

    Remove IcebergJavaAPIExamples

--- src/main/java/org/ndelnano/example/IncrementalReadMerge.java
+++ src/main/java/org/ndelnano/example/IncrementalReadMerge.java
@@ -200,5 +200,26 @@ public class IncrementalReadMerge {
                 , SCHEMA_NAME, BUSINESS_CDC_SOURCE_TABLE_NAME)).show();
 
     }
+ /*
+        // Snapshot operation values https://iceberg.apache.org/spec/#snapshots, https://iceberg.apache.org/javadoc/1.1.0/org/apache/iceberg/DataOperations.html
+        // TODO check how snapshot rollbacks are handled
+        switch (op) {
+            case DataOperations.APPEND:
+                ;
+            case DataOperations.REPLACE:
+                ;
+            case DataOperations.DELETE:
+            case DataOperations.OVERWRITE:
+                throw new IllegalStateException(
+                        String.format(
+                                "Source CDC table should not have snapshot operation: %s (snapshot id %s)",
+                                op.toLowerCase(Locale.ROOT), snapshot.snapshotId()));
+            default:
+                throw new IllegalStateException(
+                        String.format(
+                                "Cannot process unknown snapshot operation: %s (snapshot id %s)",
+                                op.toLowerCase(Locale.ROOT), snapshot.snapshotId()));
+        }
 
+         */
 }
